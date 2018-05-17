aeef51c19260cfe404884f0a4641578cfcd14066
greetty <782119927@qq.com>
2018-05-17 23:36:50 +0800

    修改代码为测试菜单控制，备份代码

--- src/main/aidl/com/posin/menudevices/Dishes.aidl
+++
@@ -1,3 +0,0 @@
-package com.posin.menudevices;
-
-parcelable Dishes;
--- src/main/java/com/posin/menuproject/MainActivity.java
+++ src/main/java/com/posin/menuproject/MainActivity.java
@@ -7,21 +7,22 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.posin.menudevices.Dishes;
+import com.posin.menudevices.constant.Dishes;
+import com.posin.menudevices.ICallback;
 import com.posin.menudevices.IMenuManage;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int MAX_SHOW_ITEMS=10;
+    private static final boolean IS_CHINESE=true;
+
     private IMenuManage mMenuManage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,34 +36,47 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()) {
             case R.id.btn_send:
                 try {
-//                    mMenuManage.setMenu("黄焖鸡鸭狗1354654...");
-//                    LinkedHashMap<String, String> menuMaps = new LinkedHashMap<>();
-//
-//                    menuMaps.put("greetty1", "lajdfljdaf");
-//                    menuMaps.put("greetty2", "哈哈哈哈");
-//                    menuMaps.put("greetty3", "7777777777777");
-//                    menuMaps.put("greetty4", "测试hashMap，....");
-//                    mMenuManage.setMenuMap(menuMaps);
+                    mMenuManage.init(MAX_SHOW_ITEMS, IS_CHINESE, new ICallback() {
+                        @Override
+                        public void success() throws RemoteException {
 
+                        }
 
-                    mMenuManage.sendDish(new Dishes("清蒸罗非鱼",88.88));
+                        @Override
+                        public void failure() throws RemoteException {
 
+                        }
 
+                        @Override
+                        public IBinder asBinder() {
+                            return null;
+                        }
+                    });
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
                 break;
             case R.id.btn_send2:
                 try {
-
-                    ArrayList<String> list = new ArrayList<>();
-                    list.add("Greetty");
-                    list.add("陈贵堂");
-                    list.add("拉到就发了点击");
-                    list.add("我是逗比");
-                    String returnName = mMenuManage.setListMenuList(list);
-                    Log.e(TAG, "return name is: " + returnName);
-
+                    ArrayList<Dishes> dishes = new ArrayList<>();
+                    for (int i=0;i<15;i++){
+                        dishes.add(new Dishes("鸡蛋炒饭"+i,1,(20+i),25));
+                    }
+                    mMenuManage.sendMenu(dishes,1314520.0);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_pay:
+                try {
+                    mMenuManage.pay(520,1314,1228.00,250.0);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_clear:
+                try {
+                    mMenuManage.clearMenu();
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
